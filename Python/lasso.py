3ecb9ce2ff03b2fde4ba09c3a7789dbd316440e0
adrien <adrien.leduque@gmail.com>
2021-11-24 18:36:01 +0100

    A and b generator

--- Python/lasso.py
+++ Python/lasso.py
@@ -11,9 +11,9 @@ def lasso(A, b, xk_1, zk_1, uk_1, lam, r, maxiter, epsilone1, epsilone2):
 
     m, n = A.shape
     I = np.eye(n)
-
+    historique_cout = []
+    
     R = lg.cholesky(np.dot(A.T, A) + r*I)
-
 
     iter = 0
     fin = False
@@ -32,7 +32,7 @@ def lasso(A, b, xk_1, zk_1, uk_1, lam, r, maxiter, epsilone1, epsilone2):
         
         cl = np.dot(xk - xk_1, xk - xk_1) + np.dot(zk - zk_1, zk - zk_1) + np.dot(uk - uk_1, uk - uk_1)
         cl = cl / np.dot(xk, xk) / np.dot(zk, zk) / np.dot(uk, uk)
-        if cl < epsilone1 and e.dot(e) < epsilone2 :
+        if np.sqrt(cl) < epsilone1 and lg.norm(e) < epsilone2 :
             fin = True
         
         if iter > maxiter:
@@ -42,33 +42,20 @@ def lasso(A, b, xk_1, zk_1, uk_1, lam, r, maxiter, epsilone1, epsilone2):
         zk_1 = zk
         uk_1 = uk
 
-    return xk, iter
+    return xk, iter, historique_cout
 
+def initialisation(m, n, p):
+    x0 = sp.rand(n, 1, density=p)
+    A = np.random.normal(size=(m,n))
+    A = A * sp.diags( 1 / np.sqrt(np.power(A, 2).sum(axis=0)).T, offsets=0, shape=(n,n) )
+    b = A * x0 + np.sqrt(0.001) * np.random.normal(size=(m, 1))
+    lambda_max = lg.norm(A.T.dot(b), ord=np.inf)
+    lam = 0.1*lambda_max
 
+    x0 = np.zeros(n)
+    z0 = np.zeros(n)
+    u0 = np.zeros(n)
+    b = b[:,0]
+    
+    return A, b, x0, z0, u0, lam
 
-m = 5
-n = 10
-p = 10/m/n
-
-x0 = sp.rand(n, 1, density=p)
-A = np.random.normal(size=(m,n))
-A = A * sp.diags( 1 / np.sqrt(np.power(A, 2).sum(axis=0)).T, offsets=0, shape=(n,n) )
-b = A * x0 + np.sqrt(0.001) * np.random.normal(size=(m, 1))
-lambda_max = lg.norm(A.T.dot(b), ord=np.inf)
-lam = 0.1*lambda_max
-
-historique_cout = []
-
-x0 = np.zeros(n)
-z0 = np.zeros(n)
-u0 = np.zeros(n)
-r = 1
-maxiter = 1000
-epsilone1 = 1e-6
-epsilone2 = 1e-4
-b = b[:,0]
-
-xk, iter = lasso(A, b, x0, z0, u0, lam, r, maxiter, epsilone1, epsilone2)
-
-plt.plot(historique_cout, label="cout")
-plt.show()
---
+++ Python/main.py
@@ -0,0 +1,28 @@
+
+# %% Import
+import numpy as np
+import numpy.linalg as lg
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
+from lasso import *
+
+# %% Set parameters
+
+m = 50
+n = 100
+p = 100/m/n
+
+r = 1
+maxiter = 1000
+epsilone1 = 1e-6
+epsilone2 = 1e-4
+
+# %% Resolution de Ax = b
+A, b, x0, z0, u0, lam = initialisation(n, m, p)
+xk, iter, historique_cout = lasso(A, b, x0, z0, u0, lam, r, maxiter, epsilone1, epsilone2)
+
+# %% Affichage
+
+plt.plot(historique_cout, label="cout")
+plt.show()
+
