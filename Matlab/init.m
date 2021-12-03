b7825a75cbc9d705d0d262473d2494632848758b
adrien <adrien.leduque@gmail.com>
2021-12-03 13:01:02 +0100

    change A generation to be sparse

--- Matlab/init.m
+++ Matlab/init.m
@@ -1,6 +1,6 @@
 function [A, b, x0, u0, z0, lambda] = init(m,n,p)
     x1 = sprand(n,1,p);
-    A = randn(m,n);
+    A = sprandn(m,n, p);
     A = A*spdiags(1./sqrt(sum(A.^2))',0,n,n);
     b = A*x1 + sqrt(0.001)*randn(m,1);
     x0 = zeros(n,1);
