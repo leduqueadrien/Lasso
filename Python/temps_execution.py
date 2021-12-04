77ea88bb4304905c88a3600fe4db769d5048a33c
adrien <adrien.leduque@gmail.com>
2021-12-04 22:19:04 +0100

    execution time

--- Python/temps_execution.py
+++ Python/temps_execution.py
@@ -1,37 +1,38 @@
 
-# %% Import
+# Import
 import numpy as np
 import numpy.linalg as lg
 import scipy.sparse as sp
 from time import time
 from lasso import *
 
-# %% Set parameters
-
-m = 1000
-n = 10000
-p = 100/m/n
-
+# Set parameters
+m = 50
+n = 100
+p = 0.1
 r = 1
 maxiter = 1000
 epsilone1 = 1e-6
 epsilone2 = 1e-4
+maxiter_temps = 50
 
-maxiter_temps = 10
-
-# %% Resolution de Ax = b
+# Resolution de Ax = b
 
 executions_time = np.zeros(maxiter_temps)
-execution_iter =np.zeros(maxiter_temps)
+execution_iter = np.zeros(maxiter_temps)
+execution_perf = np.zeros(maxiter_temps)
 
 for i in range(maxiter_temps):
-	parametres = initialisation(n, m, p)
+	parametres = initialisation(m, n, p)
 	start_time = time()
-	xk, iter, historique_cout = lasso(*parametres, r, maxiter, epsilone1, epsilone2)
+	xk, iter, historique_cout, flags = lasso(*parametres, r, maxiter, epsilone1, epsilone2)
 	executions_time[i] = time() - start_time
 	execution_iter[i] = iter
+	execution_perf[i] = historique_cout[iter-1]
     
 
-# %% Affichage
+# Affichage
 
+print("performance moyenne : {0}".format(execution_perf.mean()))
 print("temps moyen d'execution : {0}".format(executions_time.mean()))
+print("nombre moyen d'iteration : {0}".format(execution_iter.mean()))
