b0f2beea130bfce6bf359d5c4856f846ca071d30
adrien <adrien.leduque@gmail.com>
2021-12-04 22:19:36 +0100

    main update since the lasso algorithme works

--- Python/main.py
+++ Python/main.py
@@ -1,28 +1,28 @@
 
-# %% Import
+# Import
 import numpy as np
 import numpy.linalg as lg
 import scipy.sparse as sp
 import matplotlib.pyplot as plt
 from lasso import *
 
-# %% Set parameters
+# Set parameters
 
-m = 50
-n = 100
+m = 5
+n = 10
 p = 0.1
-
 r = 1
 maxiter = 1000
-epsilone1 = 1e-6
-epsilone2 = 1e-4
+epsilone1 = 1e-3
+epsilone2 = 1e-2
 
-# %% Resolution de Ax = b
+# Resolution de Ax = b
 A, b, x0, z0, u0, lam = initialisation(m, n, p)
-xk, iter, historique_cout = lasso(A, b, x0, z0, u0, lam, r, maxiter, epsilone1, epsilone2)
+xk, iter, historique_cout, flags = lasso(A, b, x0, z0, u0, lam, r, maxiter, epsilone1, epsilone2)
 
-# %% Affichage
+# Affichage
 
 plt.plot(historique_cout, label="cout")
+# plt.yscale("log")
 plt.show()
 
