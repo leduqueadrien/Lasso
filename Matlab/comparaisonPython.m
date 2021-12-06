8e7ee570f0685d0ee0c28108048f01eb2e352876
adrien <adrien.leduque@gmail.com>
2021-12-06 22:21:07 +0100

    rename file

---
+++ Matlab/comparaisonPython.m
@@ -0,0 +1,18 @@
+
+m = 50; %nombre d'individus
+n = 100; %nombre d'attributs
+p = 0.1; %densite de la matrice A
+r=1;
+maxiter=1000;
+delta=1e-6;
+delta0=1e-4;
+
+
+[A,b,x0,z0,u0,lambda]=init(m,n,p);
+csvwrite('../DataSave/A.csv', full(A))
+csvwrite('../DataSave/b.csv', b)
+tic
+[x,h,flag,iter]=lasso(A,b,x0,z0,u0,lambda,r,maxiter,delta,delta0);
+executions_time = toc;
+
+fprintf("temps moyen d'execution : %f sec", executions_time)
