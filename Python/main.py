ca6ace105aa5bb1664759ac3372ffd20a105f693
adleduque <adleduque@etu.isima.fr>
2021-12-11 12:13:56 +0100

    add plot legend and save

--- Python/main.py
+++ Python/main.py
@@ -5,11 +5,11 @@ import numpy.linalg as lg
 import scipy.sparse as sp
 import matplotlib.pyplot as plt
 from lasso import *
-
+from time import time
 # Set parameters
 
-m = 50
-n = 100
+m = 1000
+n = 10000
 p = 0.1
 r = 1
 maxiter = 1000
@@ -18,8 +18,9 @@ epsilone2 = 1e-4
 
 # Resolution de Ax = b
 A, b, x0, z0, u0, lam = initialisation(m, n, p)
+start_time = time()
 xk, iter, historique_cout, flags = lasso(A, b, x0, z0, u0, lam, r, maxiter, epsilone1, epsilone2)
-
+print(f"temps d'execution : {time() - start_time}")
 # Affichage
 
 plt.plot(historique_cout, label="cout")
